508254c00432f3700fcaf725d3348f27f5323d58
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-01-09 17:45:42 +0500

    feat: function to toggle open state on topic

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+const topicEl = document.querySelector('.topic-container');
+
+topicEl.addEventListener('click', openTopic);
+
+function openTopic() {
+  topicEl.classList.toggle('closed');
+  topicEl.classList.toggle('open');
+}
